b0e78d8d8a9f5a65c73500b55ac9230755862a59
Juan M. Casillas <juanm.casillas@gmail.com>
2020-07-27 19:20:19 +0200

    Added arduspinnerXY
    Support for two rotary, one for X, another one for Y.

---
+++ arduspinnerXY/arduspinnerXY.ino
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// ArduSpinner.ino
+// (c) Juan M. Casillas 2019
+// Implements a Dial (aka spinner) input control as Mouse input (X Axis)
+//
+// Support two rotary encoders, one for the X-axis, another one for the Y-axis
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#define WITHOUT_BUTTON 1
+
+#include <ClickEncoder.h>
+#include <Mouse.h>
+#include <TimerOne.h>
+
+#define TIMER1_INTERVAL 1000
+#define PIN_XA 2
+#define PIN_XB 3
+#define PIN_XBUTTON 4
+#define PIN_YA 5
+#define PIN_YB 6
+#define PIN_YBUTTON 7
+
+//#define RELEASE 1
+
+ClickEncoder *encoderX;
+ClickEncoder *encoderY;
+int16_t lastX, valueX;
+int16_t sign_valueX;
+int16_t abs_valueX;
+int16_t lastY, valueY;
+int16_t sign_valueY;
+int16_t abs_valueY;
+bool mouse_move;
+
+void timerIsr() {
+    encoderX->service();
+    encoderY->service();
+}
+
+void setup() {
+    encoderX = new ClickEncoder(PIN_XA, PIN_XB, PIN_XBUTTON, 1);
+    encoderX->setAccelerationEnabled(true);
+    encoderY = new ClickEncoder(PIN_YA, PIN_YB, PIN_YBUTTON, 1);
+    encoderY->setAccelerationEnabled(true);
+    
+    Timer1.initialize(TIMER1_INTERVAL);
+    Timer1.attachInterrupt(timerIsr);
+    lastX = lastY = -1;
+    valueX = valueY = 0;
+    mouse_move = false;
+
+    Mouse.begin();
+#ifndef RELEASE
+    Serial.begin(115200);
+    Serial.println("Arduspinner Started");
+#endif
+}
+
+void loop() {
+    valueX = encoderX->getValue();
+    valueY = encoderY->getValue();
+    mouse_move = false;
+
+    if (valueX != 0) {
+        lastX = valueX;
+        mouse_move = true;
+    }
+    if (valueY != 0) {
+        lastY = valueY;
+        mouse_move = true;
+    }
+    
+    if (mouse_move) {
+        Mouse.move(valueX, valueY, 0);
+
+#ifndef RELEASE
+        Serial.print("EncoderX Value: ");
+        Serial.println(valueX);
+        Serial.print("EncoderY Value: ");
+        Serial.println(valueY);
+#endif
+    }
+    
+
+}
